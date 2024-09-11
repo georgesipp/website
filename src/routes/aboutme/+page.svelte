23c8ead960b2b47d03062483e0ca7b2907ab5637
georgesipp <georgesipp@gmx.de>
2024-09-11 11:06:59 +0200

    add test

---
+++ src/routes/aboutme/+page.svelte
@@ -0,0 +1,31 @@
+<div class="flex bg-accent h-full ~xs:~m-2/12 ~xs:~gap-2/8 rounded-lg ~xs:~p-2/12">
+	<div class="ml-2 text-justify">
+		<div>
+			<h1 class="mb-4 lg:mb-24  mt-4 text-4xl lg:text-8xl">About me</h1>
+		</div>
+		<div class="flex lg:w-2/3 lg:text-3xl">
+			<div>
+				<p class="mb-12">
+					Jonas Olbrich (*1998, Recklinghausen) beschäftigt sich in seinen Arbeiten mit der
+					Performativität des Spielerischen. Er studierte Theaterwissenschaft und Kunstgeschichte an
+					der Ruhr Universität Bochum. Derzeit schließt er sein Studium mit dem Master Spiel &&
+					Objekt an der HfS Ernst Busch in Berlin ab. Jonas verknüpft Spielmechaniken, Elektronik
+					und Game Engines, um den performativen Moment digitaler und realer Spielwelten zu
+					untersuchen.
+				</p>
+				<p class="mb-12">
+					Jonas Olbrich (*1998, Recklinghausen) is a theatre maker, researching the performativity
+					of play and game. In his work he combines game mechanics, electronics and game engines to
+					investigate the performative moment of digital and actual game worlds. Jonas studied
+					theatre studies and art history at the Ruhr University Bochum. He is currently completing
+					his Master’s degree in Spiel && Objekt at the HfS Ernst Busch in Berlin. Recent activities
+					include designing a game in theatre space for the “Das digitale Foyer” project at the FFT
+					in Düsseldorf.
+				</p>
+                <p class="mb-12">
+                    Contact: contact@jonasolbrich.de
+                </p>
+			</div>
+		</div>
+	</div>
+</div>
