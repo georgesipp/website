5d8e1b8be66dff2cb295ec5cad070b81279d85ad
georgesipp <georgesipp@gmx.de>
2024-09-11 10:30:45 +0200

    switch file

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths:{
-			base: dev ? '': process.env.BASE_PATH,
+			base: process.env.NODE_ENV === 'production' ? '/personalwebsite' : '',
 		}
 	}
 };
